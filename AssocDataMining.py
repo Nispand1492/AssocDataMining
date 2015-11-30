30234a43e86ae793fdce7d874be09a7785bfac0a
Nispand1492 <nispand.mehta@gmail.com>
2015-11-30 05:01:19 -0600

    confidence calculated

--- AssocDataMining.py
+++ AssocDataMining.py
@@ -6,7 +6,7 @@ def load_dataset():
     db = pymysql.connect("localhost","root","root")
     cur = db.cursor()
     cur.execute("use associationrulemining")
-    cur.execute("select * from student")
+    cur.execute("select * from location")
     data=cur.fetchall()
     #print ("the data is")
     #print data
@@ -78,6 +78,45 @@ def apriori(dataset, minsupport=0.0):
     #print (L)
     return L, support_data
 
+def generateRules(L, support_data, min_confidence=0.0):
+    """Create the association rules
+    L: list of frequent item sets
+    support_data: support data for those itemsets
+    min_confidence: minimum confidence threshold
+    """
+    rules = []
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            print "freqSet", freqSet, 'H1', H1
+            if (i > 1):
+                rules_from_conseq(freqSet, H1, support_data, rules, min_confidence)
+            else:
+                calc_confidence(freqSet, H1, support_data, rules, min_confidence)
+    return rules
+
+
+def calc_confidence(freqSet, H, support_data, rules, min_confidence=0.0):
+    "Evaluate the rule generated"
+    pruned_H = []
+    for conseq in H:
+        conf = support_data[freqSet] / support_data[freqSet - conseq]
+        if conf >= min_confidence:
+            print freqSet - conseq, '--->', conseq, 'conf:', conf
+            rules.append((freqSet - conseq, conseq, conf))
+            pruned_H.append(conseq)
+    return pruned_H
+
+
+def rules_from_conseq(freqSet, H, support_data, rules, min_confidence=0.0):
+    "Generate a set of candidate rules"
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)):
+        Hmp1 = aprioriGen(H, m + 1)
+        Hmp1 = calc_confidence(freqSet, Hmp1,  support_data, rules, min_confidence)
+        if len(Hmp1) > 1:
+            rules_from_conseq(freqSet, Hmp1, support_data, rules, min_confidence)
+
 dataset = load_dataset()
 a,b = (apriori(dataset))
 
@@ -85,4 +124,4 @@ for x in a :
     for y in x :
         print str(y) + "::" + str(b[y]) + "\n"
 
-
+generateRules(a,b)
